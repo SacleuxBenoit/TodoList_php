a79ba6a04fcfe1f71bec8235abcda4fb91601a4f
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-15 19:41:34 +0200

    add the file register.php with a form for the email + password in it

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div>
+        <h2>Inscription</h2>
+
+        <form action="#" method="post">
+            <p>
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email">
+            </p>
+
+            <p>
+                <label for="pass">Password :</label>
+                <input type="password" id="pass" name="pass">
+            </p>
+
+            <input type="submit" value="Submit">
+            
+        </form>
+    </div>
+</body>
+</html>
