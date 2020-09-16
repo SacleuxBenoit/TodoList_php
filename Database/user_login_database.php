b5ea96197535d18ee84c4cb8a4482d76c400adf9
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-16 12:18:59 +0200

    search in the database if the email and the password exist + redirect to home.php or login.php if it doesn't exist

--- Database/user_login_database.php
+++ Database/user_login_database.php
@@ -11,4 +11,15 @@ catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
+
+$user_login = $bdd->prepare('SELECT email,pass FROM user WHERE email = :email');
+$user_login->bindParam(':email', $_POST['LoginEmail']);
+$user_login->execute();
+$donnees = $user_login->fetch();
+
+if($_POST['LoginEmail'] == $donnees['email'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
+    header('Location: ./../home.php');
+}else{
+    header('Location: ./../login.php');
+}
 ?>
