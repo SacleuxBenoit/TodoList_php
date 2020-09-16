83bd6242c523e65c4cc3fae943f8d673a9c4f019
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-16 11:54:40 +0200

    add prepared statement for push the email + password to the database in user_infos_database.php

--- Database/user_infos_database.php
+++ Database/user_infos_database.php
@@ -4,7 +4,7 @@ include('../pass.php');
 
 try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=mailBox;charset=utf8', 'root', $_SESSION['pass']);
+    $bdd = new PDO('mysql:host=localhost;dbname=TodoList;charset=utf8', 'root', $_SESSION['pass']);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(Exception $e)
@@ -12,4 +12,8 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
+$user_info = $bdd->prepare('INSERT INTO user(email,pass) VALUES(:email, :pass)');
+$user_info->bindParam(':email', $_POST['RegisterEmail']);
+$user_info->bindParam(':pass', $_POST['RegisterPass']);
+$user_info->execute();
 ?>
