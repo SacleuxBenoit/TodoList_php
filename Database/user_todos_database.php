a5108d362dfc2eb489673e8b28ceff23e26c5791
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-16 15:54:33 +0200

    register the title + task to the database in user_todos_database.php

--- Database/user_todos_database.php
+++ Database/user_todos_database.php
@@ -11,4 +11,9 @@ catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
+
+$addTodos = $bdd->prepare('INSERT INTO create_todos(title,task) VALUES(:title, :task)');
+$addTodos->bindParam(':title', $_POST['addTitle']);
+$addTodos->bindParam(':task', $_POST['addTask']);
+$addTodos->execute();
 ?>
