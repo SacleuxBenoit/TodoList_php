3a234395a3208d65c49b4d51eb02e8fceca12731
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-16 16:01:51 +0200

    add a verification for see if an input is empty in user_todos_database.php

--- Database/user_todos_database.php
+++ Database/user_todos_database.php
@@ -12,8 +12,13 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-$addTodos = $bdd->prepare('INSERT INTO create_todos(title,task) VALUES(:title, :task)');
-$addTodos->bindParam(':title', $_POST['addTitle']);
-$addTodos->bindParam(':task', $_POST['addTask']);
-$addTodos->execute();
+if(empty($_POST['addTitle']) || empty($_POST['addTask'])){
+    header('Location: ../home.php');
+}else{
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(title,task) VALUES(:title, :task)');
+    $addTodos->bindParam(':title', $_POST['addTitle']);
+    $addTodos->bindParam(':task', $_POST['addTask']);
+    $addTodos->execute();
+}
+
 ?>
