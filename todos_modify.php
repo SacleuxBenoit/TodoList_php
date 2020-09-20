e934583798028722c3ff12a59a2537c5ddc3fc2e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-20 15:51:00 +0200

    add the doctype + form in todos_modify.php (was in Database/todos_modify_database.php

---
+++ todos_modify.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Modify</h1>
+
+    <form action="./Database/todos_modify_database.php" method="post">
+
+    <p>
+        <label for="modifyTitle">Title</label>
+        <input type="text" name="modifyTitle" id="modifyTitle">
+    </p>
+
+    <p>
+        <label for="modifyTask">Task</label>
+        <textarea id="modifyTask" name="modifyTask" rows="10" cols="40"></textarea>
+    </p>
+
+        <input type="submit" value="Envoyer">
+    </form>
