96c57014a449b8e3540126bef0e583e6419c6c34
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-23 18:34:16 +0200

    veruf

--- Database/user_infos_database.php
+++ Database/user_infos_database.php
@@ -12,14 +12,23 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-if(empty($_POST['RegisterEmail']) || empty($_POST['RegisterPass'])){
-    header('Location: ../register.php');
+$find_email = $bdd->prepare('SELECT email FROM user WHERE email = :email');
+$find_email->bindParam(':email', $_POST['RegisterEmail']);
+$find_email->execute();
+$testEmail = $find_email->fetch();
+
+if($_POST['RegisterEmail'] == $testEmail['email']){
+    echo "the email address is already taken";
+}
+else if(empty($_POST['RegisterEmail']) || empty($_POST['RegisterPass'])){
+    header('Location: ../index.php');
 }else{
     $pass_hash = password_hash($_POST['RegisterPass'], PASSWORD_DEFAULT);
     $user_info = $bdd->prepare('INSERT INTO user(email,pass) VALUES(:email, :pass)');
     $user_info->bindParam(':email', $_POST['RegisterEmail']);
     $user_info->bindParam(':pass', $pass_hash);
     $user_info->execute();
+    header('Location: ../home.php');
 }
 
 ?>
