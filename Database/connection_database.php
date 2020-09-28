a73f3c9f63563931d5401bfa0d531760bc85dff1
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-28 15:55:22 +0200

    add the file connection_database.php with connection to the database in it

---
+++ Database/connection_database.php
@@ -0,0 +1,13 @@
+<?php
+    include('../pass.php');
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=TodoList;charset=utf8', 'root', $_SESSION['pass']);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+
+?>
