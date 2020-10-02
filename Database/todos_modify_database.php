cf2cb19dacf26d5d3a21ab075efb8d0d92e0fcc7
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-02 18:14:41 +0200

    delete every part with (title) in todos_modify_database.php, user_todos_database.php

--- Database/todos_modify_database.php
+++ Database/todos_modify_database.php
@@ -4,8 +4,7 @@ include('connection_database.php');
 
     $id = $_GET['id'];
     
-    $modify_todos = $bdd->prepare('UPDATE create_todos SET title = :title, task = :task WHERE id = :id');
-    $modify_todos->bindParam(':title', $_POST['modifyTitle']);
+    $modify_todos = $bdd->prepare('UPDATE create_todos SET task = :task WHERE id = :id');
     $modify_todos->bindParam(':task', $_POST['modifyTask']);
     $modify_todos->bindParam(':id', $id);
     $modify_todos->execute();
--- Database/user_todos_database.php
+++ Database/user_todos_database.php
@@ -2,11 +2,10 @@
 session_start();
 include('connection_database.php');
 
-if(empty($_POST['addTitle']) || empty($_POST['addTask'])){
+if(empty($_POST['addTask'])){
     header('Location: ../home.php');
 }else{
-    $addTodos = $bdd->prepare('INSERT INTO create_todos(title,task) VALUES(:title, :task)');
-    $addTodos->bindParam(':title', $_POST['addTitle']);
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(task) VALUES(:task)');
     $addTodos->bindParam(':task', $_POST['addTask']);
     $addTodos->execute();
     header('Location: ../home.php');
