06ff6a2a32945f207bbf65644de42c30164caaf6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-03 18:25:19 +0200

    add include to connection_database, session_start, query for delete every todos, and redirection in todos_deleteALL_database.php

---
+++ Database/todos_deleteALL_database.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include('connection_database.php');
+
+$deleteALL_todos = $bdd->query('DELETE FROM create_todos');
+
+if($deleteALL_todos){
+    header('Location: ../home.php');
+}else{
+    echo "Error deleting todos";
+}
+?>
