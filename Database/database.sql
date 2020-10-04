69f3de3a1e0b2c12ec789613f8c6a926b0078c94
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-04 18:01:38 +0200

    update database.sql with DATETIME for the table create_todos + push the current date  when we create a todo in user_todos_database.php
    .

--- Database/database.sql
+++ Database/database.sql
@@ -1,17 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Oct 04, 2020 at 12:59 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 04, 2020 at 03:57 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Database: `TodoList`
+-- Database: `todolist`
 --
 
 -- --------------------------------------------------------
@@ -20,22 +27,25 @@ SET time_zone = "+00:00";
 -- Table structure for table `create_todos`
 --
 
-CREATE TABLE `create_todos` (
-  `id` int(11) NOT NULL,
-  `task` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `create_todos`;
+CREATE TABLE IF NOT EXISTS `create_todos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `task` text NOT NULL,
+  `createdAT` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `create_todos`
 --
 
-INSERT INTO `create_todos` (`id`, `task`) VALUES
-(1, 'first task'),
-(2, 'another task\r\n'),
-(3, 'homeWork'),
-(4, 'Do the shopping'),
-(5, 'take a walk with the dogs');
-
+INSERT INTO `create_todos` (`id`, `task`, `createdAT`) VALUES
+(1, 'first task', NULL),
+(2, 'another task\r\n', NULL),
+(3, 'homeWork', NULL),
+(4, 'Do the shopping', NULL),
+(5, 'take a walk with the dogs', NULL),
+(36, 'test with DATETIME', '2020-10-04 17:55:57');
 
 -- --------------------------------------------------------
 
@@ -43,11 +53,13 @@ INSERT INTO `create_todos` (`id`, `task`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `ID` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
@@ -58,36 +70,8 @@ INSERT INTO `user` (`ID`, `email`, `pass`) VALUES
 (3, 'aze@aze.aze', '$2y$10$4rtt5O6/auu42tkOiHtKr.f9mGJUmS7CiLyPXpNbnZyUQ2dZt7joq'),
 (4, 'another@account.com', '$2y$10$e8Il7xtAF6IZrRIJg0fTte7LoM9CBj3I22/a8HlZnEFST2MaETHAm'),
 (5, 'a@a.a', '$2y$10$h5RxhyIyylmG1erP.1yoB.2qShxiE9AVdJYpV5jnqgQ1f4PnoLY46');
+COMMIT;
 
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `create_todos`
---
-ALTER TABLE `create_todos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `create_todos`
---
-ALTER TABLE `create_todos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Database/user_todos_database.php
+++ Database/user_todos_database.php
@@ -5,7 +5,7 @@ include('connection_database.php');
 if(empty($_POST['addTask'])){
     header('Location: ../home.php');
 }else{
-    $addTodos = $bdd->prepare('INSERT INTO create_todos(task) VALUES(:task)');
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(task,createdAT) VALUES(:task,NOW())');
     $addTodos->bindParam(':task', $_POST['addTask']);
     $addTodos->execute();
     header('Location: ../home.php');
