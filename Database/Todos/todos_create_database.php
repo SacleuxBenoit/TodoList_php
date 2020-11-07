7a580187083c6b0f37856fea49bafc4415ff05f5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-11-07 17:38:41 +0100

    add input type datetime-local in home.php... push the result of $_post deadline to the database in Todos_create_database.php

--- Database/Todos/todos_create_database.php
+++ Database/Todos/todos_create_database.php
@@ -5,8 +5,9 @@ include('../connection_database.php');
 if(empty($_POST['addTask'])){
     header('Location: ../../home.php');
 }else{
-    $addTodos = $bdd->prepare('INSERT INTO create_todos(task,createdAT) VALUES(:task,NOW())');
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(task,deadLine,createdAT) VALUES(:task,:deadLine,NOW())');
     $addTodos->bindParam(':task', $_POST['addTask']);
+    $addTodos->bindParam(':deadLine', $_POST['deadLine']);
     $addTodos->execute();
     header('Location: ../../home.php');
 }
