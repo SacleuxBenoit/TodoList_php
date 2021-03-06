a7c0f050e853ca356ccc66508604d13801761759
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-06 12:19:46 +0100

    get the id of the user in  :arrow_right: user_login_database.php

--- Database/User/user_login_database.php
+++ Database/User/user_login_database.php
@@ -3,13 +3,14 @@
  include('../../pass.php');
  include('../connection_database.php');
 
- $_SESSION['username'] = $_POST['LoginPseudo'];
-
- $user_login = $bdd->prepare('SELECT username,pass FROM user WHERE username = :username');
+ $user_login = $bdd->prepare('SELECT id,username,pass FROM user WHERE username = :username');
  $user_login->bindParam(':username', $_POST['LoginPseudo']);
  $user_login->execute();
  $donnees = $user_login->fetch();
 
+ $_SESSION['username'] = $_POST['LoginPseudo'];
+ $_SESSION['id_user'] = $donnees['id'];
+ 
  if($_POST['LoginPseudo'] == $donnees['username'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
      header('Location: ../../layouts/todos.php');
  }else{
