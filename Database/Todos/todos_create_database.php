ae4a9812305a95967beb97b5df135e1cf84591f6
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-06 12:23:20 +0100

    link the todos with id_user :arrow_right: todos_create_database.php

--- Database/Todos/todos_create_database.php
+++ Database/Todos/todos_create_database.php
@@ -5,7 +5,8 @@ include('../connection_database.php');
 if(empty($_POST['addTask']) || empty($_POST['deadLine'])){
     header('Location: ../../layouts/todos.php');
 }else{
-    $addTodos = $bdd->prepare('INSERT INTO create_todos(task,deadLine,createdAT) VALUES(:task,:deadLine,NOW())');
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(id_user,task,deadLine,createdAT) VALUES(:id_user,:task,:deadLine,NOW())');
+    $addTodos->bindParam(':id_user', $_SESSION['id_user']);
     $addTodos->bindParam(':task', $_POST['addTask'], PDO::PARAM_STR);
     $addTodos->bindParam(':deadLine', $_POST['deadLine'], PDO::PARAM_STR);
     $addTodos->execute();
