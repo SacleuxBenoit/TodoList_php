7716bda22b1621fcfe518dc70ee6174b05637795
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-11 16:43:13 +0100

    add verification if (LoginPseudo) exist :arrow_right: user_login_database.php

--- Database/User/user_login_database.php
+++ Database/User/user_login_database.php
@@ -8,12 +8,15 @@
  $user_login->execute();
  $donnees = $user_login->fetch();
 
- $_SESSION['username'] = $_POST['LoginPseudo'];
+ $_SESSION['username'] = $_POST['LoginPseudo'] || $_POST['SettingsPass'];
  $_SESSION['id_user'] = $donnees['id'];
  
- if($_POST['LoginPseudo'] == $donnees['username'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
-     header('Location: ../../layouts/todos.php');
- }else{
-     header('Location: ../../index.php');
+ if(isset($_POST['LoginPseudo'])){
+    if($_POST['LoginPseudo'] == $donnees['username'] && password_verify($_POST['LoginPass'],$donnees['pass'])){
+        header('Location: ../../layouts/todos.php');
+    }else{
+        header('Location: ../../index.php');
+    }
  }
+
  ?> 
