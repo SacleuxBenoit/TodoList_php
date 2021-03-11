014e402ef5fce487108905df64f7c5493e50091a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-11 23:56:18 +0100

    create the file user_settings_database.php : verify if the account exist, and redirect to the layout settings.php

---
+++ Database/User/user_settings_database.php
@@ -0,0 +1,22 @@
+<?php
+ session_start();
+ include('../../pass.php');
+ include('../connection_database.php');
+
+ $user_settings = $bdd->prepare('SELECT id,username,pass FROM user WHERE username = :username');
+ $user_settings->bindParam(':username', $_POST['SettingsPseudo']);
+ $user_settings->execute();
+ $donnees = $user_settings->fetch();
+
+ $_SESSION['username'] = $_POST['SettingsPass'];
+ $_SESSION['id_user'] = $donnees['id'];
+ 
+ if(isset($_POST['SettingsPseudo'])){
+    if($_POST['SettingsPseudo'] == $donnees['username'] && password_verify($_POST['SettingsPass'],$donnees['pass'])){
+        header('Location: ../../layouts/settings.php');
+    }else{
+        header('Location: ../../index.php');
+    }
+ }
+
+ ?> 
