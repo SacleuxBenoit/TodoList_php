b6e2a188ef05907f86665d8f5a9041f4fe127c0e
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-14 12:39:34 +0100

    verify if  settingsUsername is empty or not + add redirection :arrow_right: settings_username.php

--- Database/Settings/settings_username.php
+++ Database/Settings/settings_username.php
@@ -3,8 +3,15 @@ session_start();
 include('../../pass.php');
 include('../connection_database.php');
 
+if(!empty($_POST['settingsUsername'])){
     $send = $bdd->prepare('UPDATE user SET username = :newUsername WHERE username = :username');
     $send->bindParam(':newUsername', $_POST['settingsUsername']);
     $send->bindParam(':username', $_SESSION['usernameRegister']);
     $send->execute();
+    header('Location: ../../layouts/todos.php');
+}else{
+    header('Location: ../../index.php');
+
+}
+
 ?>
