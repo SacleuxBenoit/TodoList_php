ee69ad5f0a478bdd708f86debbf23aacd91e213f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-15 00:31:36 +0100

    create the function deleteTodos : add verification before deleting todos :arrow_right: script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function deleteAccount(){
         document.location.href="../Database/Settings/settings_delete_account.php";
     }
 }
+
+
+function deleteTodos(){
+    let result = confirm("Êtes-vous sur de vouloire supprimer toutes les todos ?");
+
+    if(result){
+        document.location.href="../Database/Todos/todos_deleteAll_database.php";
+    }
+}
