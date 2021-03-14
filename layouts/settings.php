8a1ebbb804f1b36a3777baba54252f021a9910cb
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-14 15:43:44 +0100

    :memo: add link for the first h1 to index.php :arrow_right: settings.php

--- layouts/settings.php
+++ layouts/settings.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/style_settings.css">
-    <title>Document</title>
+    <title>Settings</title>
 </head>
 <body>
 
 <header>
-    <h1>Todo List</h1>
+    <h1><a href="http://localhost:8888/test/TodoList_php/database/user/user_logout_database.php">Todo List</a></h1>
 </header>
 
 <div class="container">
