603a17c65d6dc63865cdbee088aa7b5290c4a3bd
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-15 00:37:38 +0100

    :memo: correctif : user can now delete his own todo (instead of the entiere database) :arrow_right: todos_deleteAll_database.php

--- Database/Todos/todos_deleteALL_database.php
+++ Database/Todos/todos_deleteALL_database.php
@@ -1,9 +1,11 @@
 <?php
 session_start();
 include('../connection_database.php');
-include('../pass.php');
+include('../../pass.php');
 
-$deleteALL_todos = $bdd->query('DELETE FROM create_todos');
+$deleteALL_todos = $bdd->prepare('DELETE FROM create_todos WHERE id_user = :id_user');
+$deleteALL_todos->bindParam(':id_user', $_SESSION['id_user']);
+$deleteALL_todos->execute();
 
 if($deleteALL_todos){
     header('Location: ../../layouts/todos.php');
