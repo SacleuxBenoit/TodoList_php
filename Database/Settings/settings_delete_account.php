4e9cc4af32a9686505b69ddf1860f50a56231d01
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-20 16:53:20 +0100

    add redirection to the index when an account is delete :arrow_right: settings_delete_account.php

--- Database/Settings/settings_delete_account.php
+++ Database/Settings/settings_delete_account.php
@@ -6,4 +6,5 @@ include('../connection_database.php');
 $delete_account = $bdd->prepare('DELETE FROM user WHERE username = :username');
 $delete_account->bindParam(':username', $_SESSION['usernameRegister']);
 $delete_account->execute();
+header('Location: ../../index.php');
 ?>
