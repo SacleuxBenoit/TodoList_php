62d50d9febf23acdc38cd56736dea07da635b27b
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-03-23 00:33:43 +0100

    add title for :arrow_right; index.php && todos.php

--- layouts/todos.php
+++ layouts/todos.php
@@ -9,7 +9,7 @@ include('../pass.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/style_todos.css">
-    <title>Document</title>
+    <title>TodoList</title>
 </head>
 <body>
 
