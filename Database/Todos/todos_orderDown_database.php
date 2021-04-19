ceb56dd01761b90600df1591fa39241182cac943
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-19 15:11:28 +0200

    change name of variables :arrow_right: todos_orderDown_database.php

--- Database/Todos/todos_orderDown_database.php
+++ Database/Todos/todos_orderDown_database.php
@@ -5,16 +5,16 @@ include('../../login_database.php');
 
 $id = $_GET['id_todos'];
 
-$select_order_todos = $bdd->prepare('SELECT order_todos FROM create_todos WHERE id_todos = :id_todos');
-$select_order_todos->bindParam(':id_todos', $id);
-$select_order_todos->execute();
+$select_orderDown_todos = $bdd->prepare('SELECT order_todos FROM create_todos WHERE id_todos = :id_todos');
+$select_orderDown_todos->bindParam(':id_todos', $id);
+$select_orderDown_todos->execute();
 
-$get_order_todos = $select_order_todos->fetch();
+$$get_orderDown_todos = $select_orderDown_todos->fetch();
 
-$increment_order_todos = $get_order_todos['order_todos'] + 1;
+$increment_orderDown_todos = $$get_orderDown_todos['order_todos'] + 1;
 
-$update_order_todos = $bdd->prepare('UPDATE create_todos SET order_todos = :order_todos WHERE id_todos = :id_todos');
-$update_order_todos->bindParam(':order_todos', $increment_order_todos);
-$update_order_todos->bindParam(':id_todos', $id);
-$update_order_todos->execute();
+$update_orderDown_todos = $bdd->prepare('UPDATE create_todos SET order_todos = :order_todos WHERE id_todos = :id_todos');
+$update_orderDown_todos->bindParam(':order_todos', $increment_orderDown_todos);
+$update_orderDown_todos->bindParam(':id_todos', $id);
+$update_orderDown_todos->execute();
 ?>
