af8c7215fd45313cc760329b531260d3da500109
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-22 22:20:48 +0200

    can now push (categories) into the database :arrow_right: todos_create_database.php

--- Database/Todos/todos_create_database.php
+++ Database/Todos/todos_create_database.php
@@ -6,9 +6,10 @@ include('../../login_database.php');
 if(empty($_POST['addTask'])){
     header('Location: ../../layouts/todos.php');
 }else{
-    $addTodos = $bdd->prepare('INSERT INTO create_todos(id_user,order_todos,task,createdAT) VALUES(:id_user, 1 , :task, NOW())');
+    $addTodos = $bdd->prepare('INSERT INTO create_todos(id_user,order_todos,task,categories,createdAT) VALUES(:id_user, 1 , :task, :categories, NOW())');
     $addTodos->bindParam(':id_user', $_SESSION['id_user']);
     $addTodos->bindParam(':task', $_POST['addTask'], PDO::PARAM_STR);
+    $addTodos->bindParam(':categories', $_POST['categories'], PDO::PARAM_STR);
     $addTodos->execute();
     header('Location: ../../layouts/todos.php');
 }
