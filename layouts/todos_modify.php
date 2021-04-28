742344b37daa79a84b8c3e09c47b60da3a39dab1
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-04-28 19:13:43 +0200

    user can now modify categories

--- layouts/todos_modify.php
+++ layouts/todos_modify.php
@@ -13,25 +13,35 @@ include('../login_database.php');
     <title>TodoList - Modify</title>
 </head>
 <body>
-    <?php $id = $_GET['id_todos'];
+    <?php 
+    
+        $id = $_GET['id_todos'];
 
-    $todos = $bdd->prepare('SELECT task FROM create_todos WHERE id_todos = :id');
-    $todos->bindParam(':id', $id);
-    $todos->execute();
-    $get_todos = $todos->fetch();
+        $todos = $bdd->prepare('SELECT task FROM create_todos WHERE id_todos = :id');
+        $todos->bindParam(':id', $id);
+        $todos->execute();
+        $get_todos = $todos->fetch();
     ?>
-<a href="./todos.php"><h1>Modify</h1></a>
 
-<form action="../Database/Todos/todos_modify_database.php?id_todos=<?php echo $id?>" method="post">
+    <a href="./todos.php"><h1>Modify</h1></a>
 
-    <p>
-        <label for="modifyTask">Task :</label>
-    </p>
-    <p>
-        <textarea id="modifyTask" name="modifyTask" rows="10" cols="40"><?php echo $get_todos['task']?></textarea>
-    </p>
+    <form action="../Database/Todos/todos_modify_database.php?id_todos=<?php echo $id?>" method="post">
 
-    <input type="submit" value="Envoyer">
-</form>
+        <p>
+            <label for="modifyTask">Task :</label>
+        </p>
+        <p>
+            <textarea id="modifyTask" name="modifyTask" rows="10" cols="40"><?php echo $get_todos['task']?></textarea>
+        </p>
+
+        <select name="categories" id="categories">
+            <option value='none'>none</option>
+            <option value='Monday'>Monday</option>
+            <option value='Tuesday'>Tuesday</option>
+            <option value='Wednesday'>Wednesday</option>
+        </select> 
+        
+        <input type="submit" value="Envoyer">
+    </form>
 </body>
 </html>
