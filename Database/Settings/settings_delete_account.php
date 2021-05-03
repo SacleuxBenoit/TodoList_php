636cce6709478595f3f4eff86a66acc27641114a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-03 13:30:51 +0200

    delete todos when we delete the user :arrow_right: settings_delete_account.php

--- Database/Settings/settings_delete_account.php
+++ Database/Settings/settings_delete_account.php
@@ -3,8 +3,24 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$delete_account = $bdd->prepare('DELETE FROM user WHERE username = :username');
-$delete_account->bindParam(':username', $_SESSION['usernameRegister']);
+$select_user_id = $bdd->prepare('SELECT id from user WHERE username = :username');
+$select_user_id->bindParam(':username', $_SESSION['username']);
+$select_user_id->execute();
+
+$get_user_id = $select_user_id->fetch();
+
+echo $get_user_id['id'];
+
+// Delete account 
+
+$delete_account = $bdd->prepare('DELETE FROM user WHERE id = :id');
+$delete_account->bindParam(':id', $get_user_id['id']);
 $delete_account->execute();
-header('Location: ../../index.php');
+
+// Delete todos
+
+$delete_todos = $bdd->prepare('DELETE FROM create_todos WHERE id_user = :id');
+$delete_todos->bindParam(':id', $get_user_id['id']);
+$delete_todos->execute();
+/*header('Location: ../../index.php');*/
 ?>
