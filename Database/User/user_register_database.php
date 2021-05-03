1ef273a496debfd277c8142293225f03ad1b0bf0
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-03 14:07:58 +0200

    :memo: hotfixe : add verification for see if username already exist in the database :arrow_right: user_register_database.php

--- Database/User/user_register_database.php
+++ Database/User/user_register_database.php
@@ -1,30 +1,39 @@
 <?php
- session_start();
- include('../../login_database.php');
- include('../connection_database.php');
+session_start();
+include('../../login_database.php');
+include('../connection_database.php');
 
- $_SESSION['username'] = $_POST['RegisterPseudo'];
+$_SESSION['username'] = $_POST['RegisterPseudo'];
 
 
- if(empty($_POST['RegisterPseudo']) || empty($_POST['RegisterPass'])){
-     header('Location: ../../index.php');
- }else{
-     $pass_hash = password_hash($_POST['RegisterPass'], PASSWORD_DEFAULT);
-     $user_info = $bdd->prepare('INSERT INTO user(username,pass) VALUES(:RegisterPseudo, :pass)');
-     $user_info->bindParam(':RegisterPseudo', $_POST['RegisterPseudo']);
-     $user_info->bindParam(':pass', $pass_hash);
-     $user_info->execute();
+    // Verify if username already exist in the database
 
-     $user_id = $bdd->prepare('SELECT id FROM user WHERE username = :username');
-     $user_id->bindParam(':username', $_POST['RegisterPseudo']);
-     $user_id->execute();
+    $get_username = $bdd->prepare('SELECT username FROM user WHERE username = :username');
+    $get_username->bindParam(':username', $_POST['RegisterPseudo']);
+    $get_username->execute();
 
-     $get_user_id = $user_id->fetch();
+    $verify_username = $get_username->fetch();
 
-     $_SESSION['id_user'] = $get_user_id['id'];
-     
-     header('Location: ../../layouts/todos.php');
-
- }
+    if(!$verify_username){
+        $pass_hash = password_hash($_POST['RegisterPass'], PASSWORD_DEFAULT);
+        $user_info = $bdd->prepare('INSERT INTO user(username,pass) VALUES(:RegisterPseudo, :pass)');
+        $user_info->bindParam(':RegisterPseudo', $_POST['RegisterPseudo']);
+        $user_info->bindParam(':pass', $pass_hash);
+        $user_info->execute();
+   
+        $user_id = $bdd->prepare('SELECT id FROM user WHERE username = :username');
+        $user_id->bindParam(':username', $_POST['RegisterPseudo']);
+        $user_id->execute();
+   
+        $get_user_id = $user_id->fetch();
+   
+        $_SESSION['id_user'] = $get_user_id['id'];
+        
+        header('Location: ../../layouts/todos.php');
+    }
+    else
+    {
+        header('Location ../../index.php');
+    }
 
  ?>
