8b3da8e28e9061a5adcf693fc3ac43c11133761a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-04 22:33:01 +0200

    create the file style_categories.css, and add link to it in layouts/categories.php

--- layouts/categories.php
+++ layouts/categories.php
@@ -12,7 +12,7 @@ $categories = $_GET['categories'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style_todos.css">
+    <link rel="stylesheet" href="../css/style_categories.css">
     <title>TodoList - categories</title>
 </head>
 <body>
