6c3bff41466ea23d94c584aafa8d485c6658e7ba
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-05 21:15:40 +0200

    user can update 'categories' :arrow_right: delete_categories_database.php

--- Database/Categories/delete_categories_database.php
+++ Database/Categories/delete_categories_database.php
@@ -2,4 +2,11 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
+
+echo $_GET['categories'];
+
+$delete_categories = $bdd->prepare('UPDATE create_todos SET categories = "none" WHERE categories = :categories');
+$delete_categories->bindParam(':categories', $_GET['categories']);
+$delete_categories->execute();
+header('Location: ../../layouts/todos.php');
 ?>
