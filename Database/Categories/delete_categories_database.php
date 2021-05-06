5bc4ea8f6b4f81989b7153dd955df77989094f6a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-06 18:41:20 +0200

    :memo: delete the echo for  categories :arrow_right: delete_categories_database.php

--- Database/Categories/delete_categories_database.php
+++ Database/Categories/delete_categories_database.php
@@ -3,8 +3,6 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-echo $_GET['categories'];
-
 $delete_categories = $bdd->prepare('UPDATE create_todos SET categories = "none" WHERE categories = :categories');
 $delete_categories->bindParam(':categories', $_GET['categories']);
 $delete_categories->execute();
