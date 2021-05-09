39408082a6561086f14e4e09de08392f77630314
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-09 14:26:28 +0200

    add query update for change the password :arrow_right: Settings/settings_password.php

--- Database/Settings/settings_password.php
+++ Database/Settings/settings_password.php
@@ -3,4 +3,9 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
+$update_password = $bdd->prepare('UPDATE user SET pass = :pass WHERE username = :username');
+$update_password->bindParam(':pass', $_POST['settingsPassword']);
+$update_password->bindParam(':username', $_SESSION['username']);
+$update_password->execute();
+
 ?>
