4fa9830b702706cf5c39fc9d22e57ba78f25a33f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-09 18:18:51 +0200

    :memo: user can now correctly delete categories in the database :arrow_right: delete_categories_database.php

--- Database/Categories/delete_categories_database.php
+++ Database/Categories/delete_categories_database.php
@@ -3,8 +3,13 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$delete_categories = $bdd->prepare('UPDATE create_todos SET categories = "none" WHERE categories = :categories');
+$update_categories = $bdd->prepare('UPDATE create_todos SET categories = "none" WHERE categories = :categories');
+$update_categories->bindParam(':categories', $_GET['categories']);
+$update_categories->execute();
+
+$delete_categories = $bdd->prepare('DELETE FROM categories WHERE categories = :categories');
 $delete_categories->bindParam(':categories', $_GET['categories']);
 $delete_categories->execute();
+
 header('Location: ../../layouts/todos.php');
 ?>
