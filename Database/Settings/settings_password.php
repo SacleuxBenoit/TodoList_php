ec183d88f042588ace5dc41385e0a5a1dd2aeb0e
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-09 14:30:26 +0200

    hash the password in the database :arrow_right: Settings/settings_password.php

--- Database/Settings/settings_password.php
+++ Database/Settings/settings_password.php
@@ -3,9 +3,17 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-$update_password = $bdd->prepare('UPDATE user SET pass = :pass WHERE username = :username');
-$update_password->bindParam(':pass', $_POST['settingsPassword']);
-$update_password->bindParam(':username', $_SESSION['username']);
-$update_password->execute();
+
+if(!empty($_POST['settingsPassword'])){
+    $pass_hash = password_hash($_POST['settingsPassword'], PASSWORD_DEFAULT);
+
+    $update_password = $bdd->prepare('UPDATE user SET pass = :pass WHERE username = :username');
+    $update_password->bindParam(':pass', $pass_hash);
+    $update_password->bindParam(':username', $_SESSION['username']);
+    $update_password->execute();
+    header('Location: ../../index.php');
+}else{
+    header('Location: ../../layouts/settings.php');
+}
 
 ?>
