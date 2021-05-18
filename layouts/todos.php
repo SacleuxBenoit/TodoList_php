1ff1854a591e98650eb9acff0c2cc1a1911cf624
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-18 22:24:13 +0200

    add every day of the week in the dropdown categories :arrow_right: layouts/todos.php

--- layouts/todos.php
+++ layouts/todos.php
@@ -46,6 +46,10 @@ if(empty($_SESSION['username'])){
             <option value='Monday'>Monday</option>
             <option value='Tuesday'>Tuesday</option>
             <option value='Wednesday'>Wednesday</option>
+            <option value='Thursday'>Thursday</option>
+            <option value='Friday'>Friday</option>
+            <option value='Saturday'>Saturday</option>
+            <option value='Sunday'>Sunday</option>
         </select> 
 
             <input type="submit" value="Envoyer">
