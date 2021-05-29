c8bd69d45cd62b270fa305e2d3cd1027192461bb
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-29 18:34:42 +0200

    move the class divCategories in the nav :arrow_right: layouts/todos.php, update the grid-template-area with the divCategories :arrow_right: style_todos.css

--- layouts/todos.php
+++ layouts/todos.php
@@ -55,37 +55,37 @@ if(empty($_SESSION['username'])){
             <input type="submit" value="Envoyer">
     </form>
 
-    <div class="divCategories">
-        <p>
-            <h2> Categories <button onclick="showForm()" id="buttonCategories" value="+">+</button> </h2> 
-        </p>
+    <nav class="divCategories">
+        <div>
+            <p>
+                <h2> Categories <button onclick="showForm()" id="buttonCategories" value="+">+</button> </h2> 
+            </p>
 
-        <form action="../Database/Categories/create_categories_database.php" id="formHidden" method="post" style='visibility:hidden'>
-            <input type="text" id="newCategories" name="newCategories">
-            <input type="submit" value="Submit">
-        </form>
+            <form action="../Database/Categories/create_categories_database.php" id="formHidden" method="post" style='visibility:hidden'>
+                <input type="text" id="newCategories" name="newCategories">
+                <input type="submit" value="Submit">
+            </form>
 
-        <nav>
-            <?php
-                $get_categories = $bdd->prepare('SELECT DISTINCT categories FROM create_todos WHERE id_user = :id_user ORDER BY categories');
-                $get_categories->bindParam('id_user', $_SESSION['id_user']);
-                $get_categories->execute();
-   
-                while($display_categories = $get_categories->fetch()){
-                    ?>
-                        <div id="divCategories">
-                            <ul>
-                                <li>
-                                   <h2><a href="./categories.php?categories=<?php echo $display_categories['categories'];?>"> <?php echo $display_categories['categories'] ?> </a>
-                                     | <a href="../Database/Categories/delete_categories_database.php?categories=<?php echo $display_categories['categories'] ?>"> x</h2> </a>
-                                </li>
-                            </ul>
-                        </div>
-                    <?php
-                }
-            ?>
-        </nav>
-    </div>
+                <?php
+                    $get_categories = $bdd->prepare('SELECT DISTINCT categories FROM create_todos WHERE id_user = :id_user ORDER BY categories');
+                    $get_categories->bindParam('id_user', $_SESSION['id_user']);
+                    $get_categories->execute();
+    
+                    while($display_categories = $get_categories->fetch()){
+                        ?>
+                            <div id="divCategories">
+                                <ul>
+                                    <li>
+                                    <h2><a href="./categories.php?categories=<?php echo $display_categories['categories'];?>"> <?php echo $display_categories['categories'] ?> </a>
+                                        | <a href="../Database/Categories/delete_categories_database.php?categories=<?php echo $display_categories['categories'] ?>"> x</h2> </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        <?php
+                    }
+                ?>
+        </div>
+    </nav>
 
     <div class="divTodos">
         <?php
