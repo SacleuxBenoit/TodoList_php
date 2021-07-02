c2443f8856bcc0bbd682880e2d47732831fa3636
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-02 18:22:47 +0200

    rename select_orderDown_todos to select_order_categories && order_categories to id_todos :arrow_right: todos_orderDown_database.php

--- Database/Todos/todos_orderDown_database.php
+++ Database/Todos/todos_orderDown_database.php
@@ -23,18 +23,18 @@ if(isset($_GET['id_todos'])){
 }else if(isset($_GET['order_categories'])){
     $order_categories = $_GET['order_categories'];
 
-    $select_orderDown_todos = $bdd->prepare('SELECT order_categories FROM create_todos WHERE id_todos = :order_categories');
-    $select_orderDown_todos->bindParam(':order_categories', $order_categories);
-    $select_orderDown_todos->execute();
+    $select_orderDown_categories = $bdd->prepare('SELECT order_categories FROM create_todos WHERE id_todos = :id_todos');
+    $select_orderDown_categories->bindParam(':id_todos', $order_categories);
+    $select_orderDown_categories->execute();
     
-    $get_orderDown_todos = $select_orderDown_todos->fetch();
+    $get_orderDown_todos = $select_orderDown_categories->fetch();
     
     $increment_orderDown_todos = $get_orderDown_todos['order_categories'] + 1;
     
-    $update_orderDown_todos = $bdd->prepare('UPDATE create_todos SET order_categories = :order_todos WHERE id_todos = :order_categories');
-    $update_orderDown_todos->bindParam(':order_todos', $increment_orderDown_todos);
-    $update_orderDown_todos->bindParam(':order_categories', $order_categories);
-    $update_orderDown_todos->execute();
+    $update_orderDown_categories = $bdd->prepare('UPDATE create_todos SET order_categories = :order_todos WHERE id_todos = :id_todos');
+    $update_orderDown_categories->bindParam(':order_todos', $increment_orderDown_todos);
+    $update_orderDown_categories->bindParam(':id_todos', $order_categories);
+    $update_orderDown_categories->execute();
     
     header('Location: ../../layouts/todos.php');
 }else{
