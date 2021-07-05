8794df5d0e115368934280c768cc625e334e8b46
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-05 13:07:09 +0200

    :memo: hotfixe : user can now update his username correctly :arrow_right: settings_username.php

--- Database/User/Settings/settings_username.php
+++ Database/User/Settings/settings_username.php
@@ -6,7 +6,7 @@ include('../../connection_database.php');
 if(!empty($_POST['settingsUsername'])){
     $send = $bdd->prepare('UPDATE user SET username = :newUsername WHERE username = :username');
     $send->bindParam(':newUsername', $_POST['settingsUsername']);
-    $send->bindParam(':username', $_SESSION['usernameRegister']);
+    $send->bindParam(':username', $_SESSION['username']);
     $send->execute();
     header('Location: ../../../layouts/todos.php');
 }else{
