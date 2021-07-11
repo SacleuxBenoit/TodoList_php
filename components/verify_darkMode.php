66f09901c61887114b26f9fddd69148241cd88ed
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-11 18:09:13 +0200

    update verify_darkMode.php

---
+++ components/verify_darkMode.php
@@ -0,0 +1,8 @@
+<?php
+// verify user want the darkMode
+
+$select_darkMode = $bdd->prepare('SELECT darkMode FROM user WHERE username =:username');
+$select_darkMode->bindParam(':username', $_SESSION['username']);
+$select_darkMode->execute();
+$fetch_select_darkMode = $select_darkMode->fetch();
+?>
