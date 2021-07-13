ec4a6fa621b066f90175604284751cf73218dcc3
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-07-13 15:43:59 +0200

    :memo: hotfixe : change  to  at line 11 && 21 :arrow_right: todos_orderUp_database.php

--- Database/Todos/todos_orderUp_database.php
+++ Database/Todos/todos_orderUp_database.php
@@ -8,7 +8,7 @@ if(isset($_GET['id_todos'])){
     $id_todos = $_GET['id_todos'];
 
     $select_orderUp_todos = $bdd->prepare('SELECT order_todos FROM create_todos WHERE id_todos = :id_todos');
-    $select_orderUp_todos->bindParam(':id_todos', $id);
+    $select_orderUp_todos->bindParam(':id_todos', $id_todos);
     $select_orderUp_todos->execute();
 
     $get_orderUp_todos = $select_orderUp_todos->fetch();
@@ -18,7 +18,7 @@ if(isset($_GET['id_todos'])){
     if($get_orderUp_todos['order_todos'] > 1){
         $update_orderUp_todos = $bdd->prepare('UPDATE create_todos SET order_todos = :order_todos WHERE id_todos = :id_todos');
         $update_orderUp_todos->bindParam(':order_todos', $increment_orderUp_todos);
-        $update_orderUp_todos->bindParam(':id_todos', $id);
+        $update_orderUp_todos->bindParam(':id_todos', $id_todos);
         $update_orderUp_todos->execute();
         
     }
