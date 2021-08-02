b89ca24d31b8ea74a034db1fb5f4eecd6c04af6a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-02 17:38:47 +0200

    verify if the password match with the database :arrow_right: database/user/settings.php

--- Database/User/Settings/settings_username.php
+++ Database/User/Settings/settings_username.php
@@ -3,7 +3,16 @@ session_start();
 include('../../../login_database.php');
 include('../../connection_database.php');
 
-if(!empty($_POST['settingsUsername'])){
+/* ---------------- FIND THE PASSWORD IN DATABASE ---------------- */ 
+$get_pass = $bdd->prepare('SELECT pass FROM user WHERE username =:username');
+$get_pass->bindParam(':username', $_SESSION['username']);
+$get_pass->execute();
+
+$fetch_get_pass = $get_pass->fetch();
+
+/* ---------------- VERIFY IF NEW PASSWORD MATCH + IF INPUT AREN'T EMPTY ---------------- */ 
+
+if(password_verify($_POST['verifyPassword'],$fetch_get_pass['pass']) && !empty($_POST['settingsUsername']) && !empty($_POST['verifyPassword'])){
     $send = $bdd->prepare('UPDATE user SET username = :newUsername WHERE username = :username');
     $send->bindParam(':newUsername', $_POST['settingsUsername']);
     $send->bindParam(':username', $_SESSION['username']);
@@ -11,7 +20,5 @@ if(!empty($_POST['settingsUsername'])){
     header('Location: ../../../layouts/todos.php');
 }else{
     header('Location: ../../../layouts/settings.php');
-
 }
-
 ?>
