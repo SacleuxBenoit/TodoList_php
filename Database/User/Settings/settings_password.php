1ea27956aaa5c544c9252e0383f9050fbd3acdb5
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-03 18:59:19 +0200

    user now have to confirm his password before changing it :arrow_right: settings_password.php

--- Database/User/Settings/settings_password.php
+++ Database/User/Settings/settings_password.php
@@ -3,9 +3,23 @@ session_start();
 include('../../../login_database.php');
 include('../../connection_database.php');
 
+/* ---------------- FIND THE PASSWORD IN DATABASE ---------------- */ 
+$get_pass = $bdd->prepare('SELECT pass FROM user WHERE username =:username');
+$get_pass->bindParam(':username', $_SESSION['username']);
+$get_pass->execute();
 
-if(!empty($_POST['settingsPassword'])){
-    $pass_hash = password_hash($_POST['settingsPassword'], PASSWORD_DEFAULT);
+$fetch_get_pass = $get_pass->fetch();
+
+/*if(!empty($_POST['settingsPassword'])){
+
+}else{
+    header('Location: ../../../layouts/settings.php');
+}*/
+
+// verify is password correspond to the password that is in database
+
+if(password_verify($_POST['settingsCurrentPassword'],$fetch_get_pass['pass']) && $_POST['settingsNewPassword'] == $_POST['confirmNewPassword']){
+    $pass_hash = password_hash($_POST['settingsNewPassword'], PASSWORD_DEFAULT);
 
     $update_password = $bdd->prepare('UPDATE user SET pass = :pass WHERE username = :username');
     $update_password->bindParam(':pass', $pass_hash);
@@ -13,7 +27,7 @@ if(!empty($_POST['settingsPassword'])){
     $update_password->execute();
     header('Location: ../../../index.php');
 }else{
-    header('Location: ../../../layouts/settings.php');
+   header('Location: ../../../layouts/settings.php');
 }
 
 ?>
