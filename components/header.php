7638337044c45e68e028d598b672279eced2eef0
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-21 18:03:00 +0200

    create link to index.php in the header :arrow_right: header.php && style_components_header.css

--- components/header.php
+++ components/header.php
@@ -1,6 +1,11 @@
 <header>
-    <p>
-        <a href="./settings.php">Settings</a> |
-        <a href="../Database/User/user_logout_database.php">Logout</a>
-    </p>
+    <div>
+        <p>
+            <a href="http://localhost/TodoList_php/layouts/todos.php">Todolist</a> |
+            <a href="./settings.php">Settings</a> |
+            <a href="../Database/User/user_logout_database.php"> Logout</a>
+        </p>
+
+    </div>   
+
 </header>
