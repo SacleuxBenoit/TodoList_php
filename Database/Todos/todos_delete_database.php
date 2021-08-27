811013bdd477e074fea985754d393e37148dd5c7
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-27 17:46:58 +0200

    :memo: hotfixe : change the good path for the redirection when a todos is delete :arrow_right: todos_delete_database.php

--- Database/Todos/todos_delete_database.php
+++ Database/Todos/todos_delete_database.php
@@ -8,7 +8,7 @@ if(isset($_GET['id_todos'])){
     $delete_todos->bindParam(':id', $_GET['id_todos']);
     $delete_todos->execute();
     
-    header('Location: ../../layouts/todos.php');
+    header('Location: ../../layouts/deleted_tasks.php');
 }else if($_GET['id_todos_from_categories']){
     $delete_todos = $bdd->prepare('DELETE FROM create_todos WHERE id_todos = :id');
     $delete_todos->bindParam(':id', $_GET['id_todos_from_categories']);
