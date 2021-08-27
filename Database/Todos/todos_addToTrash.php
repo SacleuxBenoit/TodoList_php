97cc10e6a1c70e1a4d529e96bb75b1d91c0f0c7c
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-27 17:44:10 +0200

    user can now add todos to trash :arrow_right: todos.php && todos_addToTrash.php

--- Database/Todos/todos_addToTrash.php
+++ Database/Todos/todos_addToTrash.php
@@ -2,4 +2,9 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
+
+$update_is_delete = $bdd->prepare('UPDATE create_todos SET is_delete = 1 WHERE id_todos =:id_todos');
+$update_is_delete->bindParam(':id_todos', $_GET['id_todos']);
+$update_is_delete->execute();
+header('Location: ../../layouts/todos.php');
 ?>
