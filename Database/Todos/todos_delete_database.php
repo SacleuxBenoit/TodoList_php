aca0c805c531a2f8a823a6c2a6d94c89f1ff6204
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-28 12:42:37 +0200

    delete useless content with id_todos_from_categories :arrow_right: todos_delete_database.php

--- Database/Todos/todos_delete_database.php
+++ Database/Todos/todos_delete_database.php
@@ -7,13 +7,8 @@ if(isset($_GET['id_todos'])){
     $delete_todos = $bdd->prepare('DELETE FROM create_todos WHERE id_todos = :id');
     $delete_todos->bindParam(':id', $_GET['id_todos']);
     $delete_todos->execute();
-    
-    header('Location: ../../layouts/deleted_tasks.php');
-}else if($_GET['id_todos_from_categories']){
-    $delete_todos = $bdd->prepare('DELETE FROM create_todos WHERE id_todos = :id');
-    $delete_todos->bindParam(':id', $_GET['id_todos_from_categories']);
-    $delete_todos->execute();
-
-    header('Location: ../../layouts/categories.php');
 }
+
+header('Location: ../../layouts/deleted_tasks.php');
+
 ?>
