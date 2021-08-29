22ff36b21257270aaabccba4ec832e197055de90
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-08-29 15:33:25 +0200

    create file whatsNew.php

---
+++ layouts/whatsNew.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include('../login_database.php');
+include('../Database/connection_database.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>What's new | Todolist</title>
+</head>
+<body>
+    
+</body>
+</html>
