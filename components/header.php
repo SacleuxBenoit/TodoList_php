5e2d38e12ec0046f2645361036921bd0b2a8210c
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-02 15:53:50 +0200

    create && display link for updateWhatsNew.php if user is an admin or a super Admin :arrow_right: header.php

--- components/header.php
+++ components/header.php
@@ -1,8 +1,27 @@
+<?php
+include('../login_database.php');
+include('../Database/connection_database.php');
+
+// verify if user is admin OR super Admin
+$verify_admin = $bdd->prepare('SELECT is_admin,is_superAdmin FROM user WHERE id =:id_user');
+$verify_admin->bindParam(':id_user', $_SESSION['id_user']);
+$verify_admin->execute();
+
+$fetch_verify_admin = $verify_admin->fetch();
+?>
 <header>
     <div>
         <p>
             <a href="http://localhost/TodoList_php/layouts/todos.php">Todolist</a> |
-            <a href="./settings.php">Settings</a> |
+            <a href="./settings.php">Settings</a>
+            <?php
+            // if user is admin OR super admin : display link to updateWhatsNew
+                if($fetch_verify_admin['is_admin'] || $fetch_verify_admin['is_superAdmin']){
+                   
+                echo '<a href="./updateWhatsNew.php">' . "| update what's new |" . "</a>";
+                   
+                }
+            ?>
             <a href="../Database/User/user_logout_database.php"> Logout</a>
         </p>
 
