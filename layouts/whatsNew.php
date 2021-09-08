78b146b83d70a952292e9a8c8bd844288320ae9d
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-08 20:24:54 +0200

    delete content :arrow_right: whatsNew.php

--- layouts/whatsNew.php
+++ layouts/whatsNew.php
@@ -25,37 +25,5 @@ include('../Database/connection_database.php');
         include('../components/navBar_categories.php');
     ?>
 
-    <div class="titleNews">
-        <h2>What's new ?</h2>
-    </div>
-
-    <div class="news">
-        <div>
-            <u>30/08/2021</u>
-
-            <p>
-                <ul>
-                    <li>- Correction of problem with the links in navBar</li>
-                    <li>- Import font for this page</li>
-                    <li>- Display the green color when a todo is check in the page todos modify</li>
-                    <li>- Starting to update darkmode</li>
-                </ul>
-            </p>
-
-        <div>
-            <u>28/08/2021 && 29/08/2021</u>
-    
-            <p>
-                <ul>
-                    <li>- Creation of the part :<a href="./deleted_tasks.php">Delete tasks</a> : once a todo is deleted, it end up <a href="./deleted_tasks.php">here</a>,
-                        the user has the possibility of permanently deleting the todo OR restoring it.
-                    </li>
-                    <li>- The 'check todos' part has been created, user can now change the color of a todo to green.</li>
-                </ul>
-            </p>
-        </div>
-    </div>
-     
-
 </body>
 </html>
