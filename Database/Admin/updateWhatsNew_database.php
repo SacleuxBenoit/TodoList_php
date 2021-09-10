e6ba0b32c25c6285b8b7a5140db7ecfb8f7f6344
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-10 23:18:57 +0200

    admin can now create content for whatsNew.php :arrow_right: updateWhatsNew_database.php

--- Database/Admin/updateWhatsNew_database.php
+++ Database/Admin/updateWhatsNew_database.php
@@ -2,4 +2,13 @@
 session_start();
 include('../../login_database.php');
 include('../connection_database.php');
-?>
+
+if(!empty($_POST['currentDate']) && !empty($_POST['new'])){
+    $create_article = $bdd->prepare('INSERT INTO whatsnew(currentdate, new) VALUES(:currentdate,:new)');
+    $create_article->bindParam(':currentdate', $_POST['currentDate']);
+    $create_article->bindParam(':new', $_POST['new']);
+    $create_article->execute();
+}
+
+header('Location: ../../layouts/whatsNew.php');
+?> 
