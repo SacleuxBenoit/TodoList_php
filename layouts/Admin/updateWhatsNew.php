0713be51d796ded80a3f4109c39e9880bd089e3a
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-12 14:46:37 +0200

    init grid-area + center text in the body :arrow_right: updateWhatsNew.css + create h1 && div with class updateWhatsNew :arrow_right: updateWhatsNew.php

--- layouts/Admin/updateWhatsNew.php
+++ layouts/Admin/updateWhatsNew.php
@@ -19,18 +19,23 @@ include('../../Database/connection_database.php');
         include('../../components/header.php');
         include('../../components/navBar_categories.php');
     ?>
-    <form action="../../Database/Admin/updateWhatsNew_database.php" method="POST">
-        <p>
-            <label for="currentDate">date</label>
-            <input type="text" name="currentDate" id="currentDate">
-        </p>
 
-        <p>
-            <label for="new">content</label>
-            <input type="text" name="new" id="new">
-        </p>
-        
-        <input type="submit" value="Submit">
-    </form>
+    <div class="updateWhatsNew">
+        <h1>Update what's new :</h1>
+        <form action="../../Database/Admin/updateWhatsNew_database.php" method="POST">
+            <p>
+                <label for="currentDate">date</label>
+                <input type="text" name="currentDate" id="currentDate">
+            </p>
+
+            <p>
+                <label for="new">content</label>
+                <input type="text" name="new" id="new">
+            </p>
+            
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
 </body>
 </html>
