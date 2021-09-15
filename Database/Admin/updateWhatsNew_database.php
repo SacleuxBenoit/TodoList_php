4e7d81c4a7ca729297de558cec37bd1165c5f49f
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-15 17:35:06 +0200

    reset margin && padding :arrow_right: settings.css + insert the id_user into table whatsnew :arrow_right: updateWhatsNew_database.php

--- Database/Admin/updateWhatsNew_database.php
+++ Database/Admin/updateWhatsNew_database.php
@@ -4,7 +4,8 @@ include('../../login_database.php');
 include('../connection_database.php');
 
 if(!empty($_POST['currentDate']) && !empty($_POST['new'])){
-    $create_article = $bdd->prepare('INSERT INTO whatsnew(currentdate, new) VALUES(:currentdate,:new)');
+    $create_article = $bdd->prepare('INSERT INTO whatsnew(id_user,currentdate, new) VALUES(:id_user,:currentdate,:new)');
+    $create_article->bindParam(':id_user', $_SESSION['id_user']);
     $create_article->bindParam(':currentdate', $_POST['currentDate']);
     $create_article->bindParam(':new', $_POST['new']);
     $create_article->execute();
