bca38c7087f33a6b558b05b6bf07308f249695bb
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-16 20:22:41 +0200

    admin can now delete news from whatsNew.php :arrow_right: deleteNew_database.php

--- Database/Admin/deleteNew_database.php
+++ Database/Admin/deleteNew_database.php
@@ -3,4 +3,12 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
+
+if(isset($_GET['id'])){
+    $deleteNew = $bdd->prepare('DELETE FROM whatsnew WHERE id = :id');
+    $deleteNew->bindParam(':id', $_GET['id']);
+    $deleteNew->execute();
+}
+header('Location: ../../layouts/whatsNew.php');
+
 ?>
