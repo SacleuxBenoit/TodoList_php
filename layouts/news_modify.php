15de95f6fbf64ab1f93c97b3aaac9794347ae8fb
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-18 18:44:54 +0200

    import font :arrow_right: news_modify.php

--- layouts/news_modify.php
+++ layouts/news_modify.php
@@ -6,6 +6,12 @@ include('../Database/connection_database.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
+     <!-- import font -->
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+    </style>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
