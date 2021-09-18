e52db991af0e0e3dfaee8cbcf5ecd521e4607475
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-18 17:58:43 +0200

    create file news_modify.css

--- layouts/news_modify.php
+++ layouts/news_modify.php
@@ -9,6 +9,7 @@ include('../Database/connection_database.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/news_modify.css">
     <link rel="stylesheet" href="../css/css_components/header.css">
     <link rel="stylesheet" href="../css/css_components/navBar.css">
     <title>Document</title>
