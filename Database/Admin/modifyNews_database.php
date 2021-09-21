e883ddc19179651458ff001adcf0df412d6930a5
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-09-21 17:26:02 +0200

    :memo: verify if input modifyDate && modifyNews aren't empty :arrow_right: modifyNews_database.php

--- Database/Admin/modifyNews_database.php
+++ Database/Admin/modifyNews_database.php
@@ -3,7 +3,7 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-if(isset($_SESSION['id_news'])){
+if(isset($_SESSION['id_news']) && !empty($_POST['modifyDate']) && !empty($_POST['modifyNews'])){
     $modify_todos = $bdd->prepare('UPDATE whatsnew SET currentdate = :modifyDate, new = :modifyNews WHERE id = :id_news');
     $modify_todos->bindParam(':modifyDate', $_POST['modifyDate'], PDO::PARAM_STR);
     $modify_todos->bindParam(':modifyNews', $_POST['modifyNews'], PDO::PARAM_STR);
