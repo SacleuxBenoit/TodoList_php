cf18d6454a99c1d371a505d0d94a8938ea338bed
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-10-16 17:50:38 +0200

    :memo: hotfixe : correction of misspelling between (new && news) :arrow_right: createNews_database.php

--- Database/Admin/createNews_database.php
+++ Database/Admin/createNews_database.php
@@ -3,11 +3,11 @@ session_start();
 include('../../login_database.php');
 include('../connection_database.php');
 
-if(!empty($_POST['currentDate']) && !empty($_POST['new'])){
-    $create_article = $bdd->prepare('INSERT INTO whatsnew(id_user,currentdate, new) VALUES(:id_user,:currentdate,:new)');
+if(!empty($_POST['currentDate']) && !empty($_POST['news'])){
+    $create_article = $bdd->prepare('INSERT INTO whatsnew(id_user,currentdate, news) VALUES(:id_user,:currentdate,:news)');
     $create_article->bindParam(':id_user', $_SESSION['id_user']);
     $create_article->bindParam(':currentdate', $_POST['currentDate'], PDO::PARAM_STR);
-    $create_article->bindParam(':new', $_POST['new'], PDO::PARAM_STR);
+    $create_article->bindParam(':news', $_POST['news'], PDO::PARAM_STR);
     $create_article->execute();
 }
 
