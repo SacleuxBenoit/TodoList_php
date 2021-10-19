d478cf632160263afd525e477818d7e53687a917
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-10-19 16:06:36 +0200

    move news.css + news_modify.css + createNews.css in the folder css/news + change the css link in :arrow_right: createNews.php, news_modify.php && news.php

--- layouts/Admin/news_modify.php
+++ layouts/Admin/news_modify.php
@@ -17,7 +17,7 @@ $_SESSION['id_news'] = $_GET['id_news'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/news_modify.css">
+    <link rel="stylesheet" href="../../css/news/news_modify.css">
     <link rel="stylesheet" href="../../css/css_components/header.css">
     <link rel="stylesheet" href="../../css/css_components/navBar.css">
     <title>Document</title>
