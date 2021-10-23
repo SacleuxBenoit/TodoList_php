2edd3b529c105dbf5b5c34c65eb25801a5a827b6
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-10-23 12:08:05 +0200

    user can now choose between 2 modes : normal || daily when they want to create a category :arrow_right: create_categories_database.php

--- Database/Categories/create_categories_database.php
+++ Database/Categories/create_categories_database.php
@@ -9,11 +9,12 @@ $select_categories->execute();
 
 $verify_categories = $select_categories->fetch();
 
-// verify if input newCategories isn't empty && if it doesn't exist in the database 
+// verify if input newCategories isn't empty && if it doesn't exist in the database if that doesn't exist : create a new category
 if(!empty($_POST['newCategories']) && $verify_categories['categories'] !== $_POST['newCategories']){
-    $create_categories = $bdd->prepare('INSERT INTO categories(id_user,categories) VALUES(:id_user, :categories)');
+    $create_categories = $bdd->prepare('INSERT INTO categories(id_user,categories,mode) VALUES(:id_user, :categories,:mode)');
     $create_categories->bindParam(':id_user', $_SESSION['id_user']);
     $create_categories->bindParam(':categories', $_POST['newCategories'], PDO::PARAM_STR);
+    $create_categories->bindParam(':mode', $_POST['categoryMode'], PDO::PARAM_STR);
     $create_categories->execute();
 }
 header('Location: ../../layouts/todos.php');
