db77292d296b981ec7a255578de79d7f3fa3c773
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-11-15 15:17:16 +0100

    :memo: solve problem by deleting (mode) in the database :arrow_right: Categories/create_categories_database.php

--- Database/Categories/create_categories_database.php
+++ Database/Categories/create_categories_database.php
@@ -11,10 +11,9 @@ $verify_categories = $select_categories->fetch();
 
 // verify if input newCategories isn't empty && if it doesn't exist in the database if that doesn't exist : create a new category
 if(!empty($_POST['newCategories']) && $verify_categories['categories'] !== $_POST['newCategories']){
-    $create_categories = $bdd->prepare('INSERT INTO categories(id_user,categories,mode) VALUES(:id_user, :categories,:mode)');
+    $create_categories = $bdd->prepare('INSERT INTO categories(id_user,categories) VALUES(:id_user, :categories)');
     $create_categories->bindParam(':id_user', $_SESSION['id_user']);
     $create_categories->bindParam(':categories', $_POST['newCategories'], PDO::PARAM_STR);
-    $create_categories->bindParam(':mode', $_POST['categoryMode'], PDO::PARAM_STR);
     $create_categories->execute();
 }
 header('Location: ../../layouts/todos.php');
