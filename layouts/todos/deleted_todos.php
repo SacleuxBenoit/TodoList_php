c48daddcf5cc62335225d24eaa2ecd79a637c83e
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-02-08 22:47:14 +0100

    :memo: hotfixe : solve problem with grid in :arrow_right: deleted_todos.php + update the rename of deleted_tasks to deleted_todos.css

--- layouts/todos/deleted_todos.php
+++ layouts/todos/deleted_todos.php
@@ -15,7 +15,7 @@ include('../../Database/connection_database.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/deleted_tasks.css">
+    <link rel="stylesheet" href="../../css/deleted_todos.css">
     <link rel="stylesheet" href="../../css/css_components/header.css">
     <link rel="stylesheet" href="../../css/css_components/navBar.css">
     <title>deleted tasks | TodoList</title>
